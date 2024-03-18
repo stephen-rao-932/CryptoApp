ebca9bfdfbdd0f431b88d5cb1980df1a0eda3aac
Stephen_Prakash_Rao <sokttiroy@gmail.com>
2024-03-18 19:23:41 +0600

    Login screen completed

--- app/src/main/java/co/raogroup/cryptoapp/MainActivity.java
+++ app/src/main/java/co/raogroup/cryptoapp/MainActivity.java
@@ -1,18 +1,29 @@
 package co.raogroup.cryptoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import co.raogroup.cryptoapp.databinding.ActivityIntoBinding;
+
 public class MainActivity extends AppCompatActivity {
 
 
+  ActivityIntoBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_into);
+    binding = ActivityIntoBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
 
+
+    binding.imageView.setOnClickListener(v -> {
+
+      startActivity(new Intent(MainActivity.this,Login_activity.class));
+
+    });
 
 
   }
