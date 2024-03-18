14708143bbfd0dc3feb21c8d611fe7e15ca84f64
Stephen_Prakash_Rao <sokttiroy@gmail.com>
2024-03-18 11:39:19 +0600

    Logo added and Splash screen done

--- app/src/main/java/co/raogroup/cryptoapp/MainActivity.java
+++ app/src/main/java/co/raogroup/cryptoapp/MainActivity.java
@@ -1,18 +1,20 @@
 package co.raogroup.cryptoapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
+
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_into);
 
-    Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
+
 
   }
+
 }
